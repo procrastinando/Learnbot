a7306ae95a4b6c818c135661c984e7b31724424f
procrastinando <74340724+procrastinando@users.noreply.github.com>
2023-03-21 00:11:19 +0800

    Add files via upload

---
+++ learnbot/add_jobs.py
@@ -0,0 +1,41 @@
+import pytesseract
+from PIL import Image
+import json
+import os
+import string
+import re
+
+def list_files(directory):
+    for file in os.listdir(directory):
+        print(file)
+
+def add_job(user, data_file, new_job):
+    with open(data_file, 'r') as f:
+        data = json.load(f)
+    data[user]['jobs'].append(new_job)
+    with open(data_file, 'w') as f:
+        json.dump(data, f)
+
+def ocr_from_image(image_path):
+    image = Image.open(image_path)
+    text = pytesseract.image_to_string(image)
+    return text
+
+images_path = "images/"
+data_file = "data.json"
+user = input("Insert user ID: ")
+lang = input("Insert Job name: ")
+target_score = input("Insert target score: ")
+difficulty = input("Insert difficulty: ")
+
+list_images = os.listdir(images_path)
+list_images.sort()
+
+for i in list_images:
+    text = ocr_from_image(images_path + i).replace('\n', ' ')
+    text_list = text.split('.')
+    text_list.pop(-1)
+    for j in text_list:
+        if j != "":
+            new_job = {"lang": lang, "text": j.lstrip(), "target_score": target_score, "current_score": 0.0, "difficulty": difficulty}
+            add_job(user, data_file, new_job)
